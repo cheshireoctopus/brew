af3407b1008e6e15d956b23facb389d6d98b05f9
cheshireoctopus <chandlermoisen@gmail.com>
2013-09-15 22:49:16 -0400

    Initial

---
+++ main.rb
@@ -0,0 +1,94 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_support/all'
+require 'active_record'
+
+# AUTHENTICATION
+require 'digest/sha2'
+
+# FOR LOGGED-IN USER
+enable :sessions
+
+ActiveRecord::Base.establish_connection(
+    :adapter => "postgresql",
+    :host => "localhost",
+    :username => "Chandler",
+    :password => "",
+    :database => "brew"
+)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+require_relative './models/brewery'
+require_relative './models/beer'
+
+
+### Routes ###
+
+# Index Page
+get '/' do
+  redirect '/home'
+end
+
+# Index Page of Breweries
+get '/home' do
+  @brew_list = Brewery.order("created_at DESC").all
+  erb :home
+end
+
+# New Brewery Page
+get "/brewery/new" do
+  erb :new_brewery
+end
+
+# Create New Brewery
+post "/brewery/new" do
+  brewery = Brewery.new(params[:brewery])
+  brewery.save
+  redirect '/home'
+end
+
+# View Individual Brewery
+get '/brewery/:id' do
+  id = params[:id].to_i
+  @brewery = Brewery.find(id)
+  erb :brewery
+end
+
+# Edit Brewery
+get '/brewery/:id/edit' do
+  id = params[:id].to_i
+  @brewery = Brewery.find(id)
+  erb :edit_brewery
+end
+
+# Update Brewery
+post '/brewery/:id' do
+  id = params[:id].to_i
+  brewery = Brewery.find(id)
+  brewery.update_attributes(params[:brewery])
+  redirect '/home'
+end
+
+# DELETE BREWERY
+post '/brewery/:id/delete' do
+  id = params[:id].to_i
+  brewery = Brewery.find(id)
+  brewery.destroy
+  redirect '/home'
+end
+
+# GET NEW BEER
+get '/brewery/:id/new_beer' do
+  id = params[:id]
+  @brewery = Brewery.find(id)
+  erb :new_beer
+end
+
+# POST NEW BEER
+post '/brewery/:id/new_beer' do
+  id = params[:id].to_i
+  brewery = Brewery.find(id)
+  brewery.beers << Beer.new(params[:beer])
+  redirect '/home'
+end
---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE breweries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    state VARCHAR(30) NOT NULL
+    created_at TIMESTAMP DEFAULT now()
+);
+
+  CREATE TABLE beers (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(30) NOT NULL,
+      style VARCHAR(30) NOT NULL,
+      abv DECIMAL(4,2) NOT NULL,
+      url TEXT  NOT NULL,
+      brewery_id SERIAL,
+      FOREIGN KEY (brewery_id) REFERENCES breweries(id)
+  );
+
